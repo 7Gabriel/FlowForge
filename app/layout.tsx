1e5967a2f7853c4ad5f031455e3907ed6ed909f9
7Gabriel <joao1.gabriel@hotmail.com>
2026-01-17 19:10:45 -0300

    move images to app

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
       <body className={inter.className}>
